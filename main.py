65938560172b284e741fbec45d51c41c62435e9c
jpoma60408437 <60408437@continental.edu.pe>
2026-06-30 18:54:54 -0500

    Restaurar routers eliminados por accidente

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    app.include_router(route_auth.router)
+app.include_router(route_auth.router)
 app.include_router(route_cuentas.router)
 app.include_router(route_operaciones.router)
 app.include_router(route_creditos.router)
